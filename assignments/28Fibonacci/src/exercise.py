78cd573b1d67159de757b5f87222b64d5e8a4580
LuisFernandoGonzalezCortes <88851274+LuisFernandoGonzalezCortes@users.noreply.github.com>
2021-09-10 23:47:35 -0500

    Update exercise.py

--- assignments/28Fibonacci/src/exercise.py
+++ assignments/28Fibonacci/src/exercise.py
@@ -1,7 +1,17 @@
 
 def main():
-    #escribe tu código abajo de esta línea
-    pass
-
+    x = int(input("Enter a number: "))
+    y=1
+    z=1
+    print ("0")
+    print ("1")
+    print ("1")
+    i=0
+    while i<x:
+        num=y+z
+        y=z
+        z=num
+        print (str(num))
+        i+=1
 if __name__=='__main__':
     main()
